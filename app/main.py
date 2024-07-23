6b63a0d4b304f1d5268d41dff86d3f5733df24a6
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-07-23 12:42:08 +0200

    implement all changes py-car-wash-station

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ class Car:
                  comfort_class: int,
                  clean_mark: int,
                  brand: str) -> None:
-        self.comfort_class = comfort_class
-        self.clean_mark = clean_mark
+        self.comfort_class = comfort_class if 1 <= comfort_class <= 7 else 1
+        self.clean_mark = clean_mark if 1 <= clean_mark <= 10 else 1
         self.brand = brand
 
 
@@ -14,9 +14,14 @@ class CarWashStation:
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int) -> None:
-        self.distance_from_city_center = distance_from_city_center
+        self.distance_from_city_center = (
+            distance_from_city_center
+            if 1.0 <= distance_from_city_center <= 10.0
+            else 1.0
+        )
         self.clean_power = clean_power
-        self.average_rating = average_rating
+        self.average_rating = average_rating if 1.0 <= average_rating <= 5.0 \
+            else 1.0
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list) -> float:
