2b84f990f7bb13621632fc3e59082ea383f2e7e8
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-07-23 11:03:54 +0200

    implement py-car-wash-station with flake 8

--- app/main.py
+++ app/main.py
@@ -24,8 +24,10 @@ class CarWashStation:
         return round(price, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark) *
-                     (self.average_rating / self.distance_from_city_center), 1)
+        return round(
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * (self.average_rating / self.distance_from_city_center), 1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
